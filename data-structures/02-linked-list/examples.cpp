15ed50ae1bab2548f199f14b31ad4181f117faef
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-07-23 15:54:45 +0300

    docs: add linked list examples placeholder

---
+++ data-structures/02-linked-list/examples.cpp
@@ -0,0 +1,20 @@
+/*
+===============================================================================
+                                Examples
+===============================================================================
+
+Examples for the Singly Linked List module will be added as the project grows.
+
+Current demonstrations can be found in:
+
+- implementation.cpp
+- README.md
+- exercises.md
+
+===============================================================================
+*/
+
+int main()
+{
+    return 0;
+}
